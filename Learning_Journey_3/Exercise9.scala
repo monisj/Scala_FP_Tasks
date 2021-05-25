d97edb0b6ae5fd27f4dc66c120880a8b7e412da3
monisj <koolmonis@gmail.com>
2021-05-25 22:28:58 +0500

    Added Exercise 9

--- Learning_Journey_3/Exercise9.scala
+++ Learning_Journey_3/Exercise9.scala
@@ -2,14 +2,13 @@ object Exercise9{
     def main(args: Array[String]):Unit =  {
 
 
-    def implcit(a:Any,b:Any):Unit = {
-        implicit def any_input(a:Any,b:Any):String = {
-            return a.toString()+b.toString()
+    def implcit(a:Any):Unit = {
+        implicit def any_input(a:Any):String = {
+            return a.toString()
             
         }
-        val cat = any_input(a,b)
+        val cat = any_input(a)
         println(s"Number converted to string = ${cat}")
-        println(s"Number converted to string = ${a}")
 }
-        implcit(36,46)}
+        implcit(36)}
 }
